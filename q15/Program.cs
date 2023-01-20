154817ca1394ba0c0cb07c3704f0da4e0471e277
Sifana K S <sifana.ks@experionglobal.local>
2023-01-20 15:11:00 +0530

    fourthcommit20jan

---
+++ q15/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+int[] arr = new int[10];
+for (int i = 0; i< 5;i++)
+{
+    Console.WriteLine("enter wheight");
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+    if (arr[i] >= 24)
+    {
+        var total = (arr[i] - 24) * 15;
+        Console.WriteLine($"overwheight fare total={total}");
+    }
+    else
+    {
+        Console.WriteLine("no overwheight");
+    }
+}
+Console.WriteLine("sorted order :");
+for (int i = 0; i < 5; i++)
+{
+    for(int j = 0;j < 5;j++)
+    {
+        if (arr[i] > arr[j])
+        { 
+        int temp = arr[i];
+            arr[i] = arr[j]; 
+            arr[j] = temp;  
+        }
+    }
+
+}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{arr[i]}");
+}
