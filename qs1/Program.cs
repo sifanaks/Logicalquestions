988f5ff0140c8b085aed9e992cd450f5d797e384
Sifana K S <sifana.ks@experionglobal.local>
2023-01-20 14:59:49 +0530

    frstcommit20jan

---
+++ qs1/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+String[] arr = new String[5];
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("enter passenger name");
+    arr[i] = Console.ReadLine();
+}
+Console.WriteLine("Alphabetical order");
+
+Array.Sort(arr);
+foreach(string str in arr)
+{
+    Console.WriteLine(str); 
+}
+
+Console.WriteLine("reverse order");
+Array.Reverse(arr);
+foreach (string str in arr)
+{
+    Console.WriteLine(str);
+}
+
+Console.WriteLine("enter name for search");
+var name = Console.ReadLine();
+if (arr.Contains(name)==true)
+{
+    Console.WriteLine($" {name} exists in the array");
+}
+else
+{
+    Console.WriteLine("not exists");
+}
+foreach (var str in arr)
+{
+    Console.WriteLine(str);
+    //int length = str.Length;
+    Console.WriteLine($"length ={str?.Length}");
+}
+foreach (string str in arr)
+{
+   string[] arr1= str.Split(' ');
+    foreach (string st in arr1)
+    {
+        Console.WriteLine(st);
+    }
+}
+for(int i = 0;i < 5;i++)
+{
+    for (int j = i+1 ; j < 5; j++)
+    {
+        if (arr[i] == arr[j]) 
+        {
+            Console.WriteLine($"duplicate of {arr[i]} at {j}");
+        }
+    }
+}
+
+
+
+
+
---
+++ qs2/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("enter the time in hours!");
+Console.WriteLine("enter rate");
+int g = Convert.ToInt32(Console.ReadLine());
+double h = Convert.ToDouble(Console.ReadLine());
+if (6 < g && g < 9)
+{
+    h = h * 1.10;
+    Console.WriteLine("new rate" + h);
+}
+else if (9 < g && g < 17)
+{
+    h = h * 1.20;
+    Console.WriteLine("new rate" + h);
+}
+else if (5 < g && g < 23)
+{
+    h = h * 1.07;
+    Console.WriteLine("new rate" + h);
+}
+else if (g == 23 || g == 0 || (g >= 1 && g <= 6))
+{
+    h = h * 1.05;
+    Console.WriteLine("new rate" + h);
+}
+else
+{
+    Console.WriteLine("invalid entry");
+}
