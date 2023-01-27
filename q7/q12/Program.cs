b2b68c85d8360b942cef25bf1ae8bab83d9de635
Sifana K S <sifana.ks@experionglobal.local>
2023-01-27 10:11:11 +0530

    5th commit

---
+++ q7/q12/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+String[] arr1;
+String[] arr2;
+String[] arr3 = new String[20];
+arr1 = new String[]{ "sifana","dona","Varsha","fathima"};
+arr2 = new String[] { "Dubai", "UK", "US", "Malaysia" };
+var mylist = new List<string>();
+for (int i = 0; i < arr1.Length; i++)
+{
+    mylist.Add(arr1[i]);
+    mylist.Add(arr2[i]);
+}
+ 
+arr3=mylist.ToArray();
+foreach (string s in arr3)
+{
+    Console.WriteLine(s);
+}
--- q8/Program.cs
+++ q8/Program.cs
@@ -1,12 +1,11 @@
-﻿
-int[] arr = new int[5];
-string[] arr2 = new string[5];
-for (int i = 0; i < 5; i++)
+﻿int[] arr = new int[3];
+string[] arr2 = new string[3];
+for (int i = 0; i < 3; i++)
 {
     Console.WriteLine("enter flight fare");
      arr[i] = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine("enter destination name");
-     arr2[i] = Console.ReadLine();  
+     arr2[i] = Console.ReadLine();
 }
 Console.WriteLine("enter a number");
 int num = Convert.ToInt32(Console.ReadLine());
